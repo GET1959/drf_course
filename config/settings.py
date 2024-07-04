6fd5df385234067816fbc0c36b821261b5c83592
gennady <topchiev@mail.ru>
2024-07-04 19:49:10 +0300

    added docker

--- config/settings.py
+++ config/settings.py
@@ -75,11 +75,11 @@ REST_FRAMEWORK = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'HOST': os.getenv("HOST"),
-        'PORT': os.getenv("DB_PORT"),
-        'PASSWORD': os.getenv("PSQL_PASSWORD"),
+        'NAME': os.getenv("POSTGRES_DB"),
+        'USER': os.getenv("POSTGRES_USER"),
+        'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
+        'HOST': os.getenv("POSTGRES_HOST"),
+        'PORT': os.getenv("POSTGRES_PORT"),
     }
 }
 
